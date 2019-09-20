64574e7d51e995b4a678f162d0bc7fda277d437a
yunnysunny <yunnysunny@gmail.com>
2019-09-20 14:39:48 +0800

    add some benchmarks with HTTP 1.1 and1.0

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const isomorphicFetch = require('isomorphic-fetch')
 const superagent = require('superagent')
 const ky = require('ky-universal')
 const request = require('request')
+const { Agent } = require('http')
 
 const badDataError = new Error('ERROR: incorrect data')
 
@@ -23,7 +24,7 @@ const fixtures = {
 }
 
 const addTestCases = (suite, options) => {
-  suite.add('http.request', {
+  suite.add('http.request with default agent', {
     defer: true,
     fn: defer =>
       http.get(options.uri, res => {
@@ -38,6 +39,57 @@ const addTestCases = (suite, options) => {
           throw badDataError
         })
       })
+  })
+
+  suite.add('http.request with http 1.1', {
+    defer: true,
+    fn: defer =>
+      http.get(options.uri, { agent: new Agent({ keepalive: true }) }, res => {
+        let body = ''
+        res.on('data', data => {
+          body += data
+        })
+        res.on('end', () => {
+          if (body === fixtures[options.size]) {
+            return defer.resolve(body)
+          }
+          throw badDataError
+        })
+      })
+  })
+
+  suite.add('http.request with http 1.0', {
+    defer: true,
+    fn: defer =>
+      http.get(options.uri, { agent: false }, res => {
+        let body = ''
+        res.on('data', data => {
+          body += data
+        })
+        res.on('end', () => {
+          if (body === fixtures[options.size]) {
+            return defer.resolve(body)
+          }
+          throw badDataError
+        })
+      })
+  })
+
+  suite.add('http.request with http 1.0 and nodelay', {
+    defer: true,
+    fn: defer =>
+      http.get(options.uri, { agent: false }, res => {
+        let body = ''
+        res.on('data', data => {
+          body += data
+        })
+        res.on('end', () => {
+          if (body === fixtures[options.size]) {
+            return defer.resolve(body)
+          }
+          throw badDataError
+        })
+      }).setNoDelay(true)
   })
 
   suite.add('axios', {
